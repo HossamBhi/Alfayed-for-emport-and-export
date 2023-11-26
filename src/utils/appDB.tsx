9df2de5011649a2a3594a9a111d4602c74dfb9e2
Hossam Bhi Pasha <hossambasha9900@gmail.com>
2023-11-26 23:44:30 +0200

    Add redux

---
+++ src/utils/appDB.tsx
@@ -0,0 +1,18 @@
+import {t} from 'i18next';
+
+export const _getAppThemes = () => [
+  {id: 1, name: t('defaultTheme')},
+  {id: 2, name: t('darkTheme')},
+  {id: 3, name: t('lightTheme')},
+];
+
+export type ThemeProps = {
+  id: string | number;
+  name: string;
+  rtl?: 0 | 1;
+  code?: string;
+};
+export const _getAppLanguages = () => [
+  // {value: 'ar', label: t('arabic'), direction: 'rtl'},
+  // {value: 'en', label: t('english'), direction: 'ltr'},
+];
